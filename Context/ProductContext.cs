b22d2002275b606070ddb988e67908380b93ad85
Gabrielle Lattanzi <g.lattanzi26@gmail.com>
2024-10-22 14:57:18 -0300

    first commit

---
+++ Context/ProductContext.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hangfire.Context
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+            _random = new Random();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        private readonly Random _random;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public Product GetRandom()
+        {
+            return Products.ElementAt(_random.Next(100));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+using Hangfire;
+using Hangfire.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProductContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlackFridayConnection")));
+
+// Add Hangfire services
+builder.Services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+
+builder.Services.AddHangfireServer();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseHangfireDashboard();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHangfireDashboard();
+});
+
+
+app.Run();
